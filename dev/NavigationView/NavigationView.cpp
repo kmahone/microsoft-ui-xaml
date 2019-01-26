c5b3c8b4937025e91f16342ea08197581c376e4a
Yongqiang Li <yongql@microsoft.com>
2019-01-25 22:45:34 -0800

    Keep USE_INSIDER_SDK consistent with recent master. USE_INTERNAL_SDK for commandbarflyout and slider/buttonInteraction test to filter them out.

--- dev/NavigationView/NavigationView.cpp
+++ dev/NavigationView/NavigationView.cpp
@@ -2730,7 +2730,7 @@ void NavigationView::OnIsPaneOpenChanged()
 
     if (SharedHelpers::IsThemeShadowAvailable())
     {
-#ifdef USE_INTERNAL_SDK
+#ifdef USE_INSIDER_SDK
         if (auto splitView = m_rootSplitView.get())
         {
             if (auto paneRoot = splitView.Pane())
